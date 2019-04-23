cbcd21b91c287d39491c1af37fe014b2fda779df
Justin van Koten <jvankoten@skupos.com>
2019-04-23 00:13:47 -0700

    init / setup skeleton

---
+++ controller.rb
@@ -0,0 +1,14 @@
+class LogParserController
+
+    def initialize
+        @log_file = LogFile.new
+        @current_view = FileDialogView.new
+    end
+
+    def run
+        while true do
+            @current_view.display
+            break
+        end
+    end
+end
