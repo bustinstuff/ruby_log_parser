02a5c65bcc8a05446da5e857aea7baff87d248cb
Justin van Koten <jvankoten@skupos.com>
2019-04-29 22:27:12 -0700

    work

--- controller.rb
+++ controller.rb
@@ -3,12 +3,55 @@ class LogParserController
     def initialize
         @log_file = LogFile.new
         @current_view = FileDialogView.new
+        @current_view.clear_display
+        @current_view.set_cursor
+        @current_view.display @log_file
     end
 
     def run
-        while true do
-            @current_view.display
-            break
+        while user_input = $stdin.getch do
+            begin
+                while next_chars = $stdin.read_nonblock(10) do
+                    user_input = "#{user_input}#{next_chars}"
+                end
+            rescue IO::WaitReadable
+            end 
+            if @current_view.quittable? && user_input == 'q'
+                break
+            else
+                parse_input user_input
+            end
         end
+    end
+
+    def parse_input user_input
+        case user_input
+            when "\n"
+            when "\e[A"
+                case @current_view.class.to_s
+                    when "FileDialogView"
+                        file_dialog_move (-1)
+                end
+            when "\e[B"
+                case @current_view.class.to_s
+                    when "FileDialogView"
+                        file_dialog_move 1
+                end
+            when "\e[C"
+            when "\e[D"
+            else
+        end
+    end
+
+    def file_dialog_move increment
+        @log_file.directory_index += increment
+
+        if @log_file.directory_index < @log_file.list_start
+            @log_file.list_start = @log_file.directory_index - $stdin.winsize[0] + 3
+        elsif @log_file.directory_index > @log_file.list_start + $stdin.winsize[0] - 3
+            @log_file.list_start = @log_file.directory_index
+        end
+
+        @current_view.update @log_file
     end
 end
--- view.rb
+++ view.rb
@@ -1,9 +1,61 @@
+require "io/console"
+
 class BasicView
+    def clear_display
+        print "\e[2J"
+    end
+
+    def set_cursor row = 1, column = 1
+        print "\e[#{row};#{column}H"
+    end
+
+    def red text
+        "\e[31;40m#{text}\e[0m"
+    end
+
+    def center text
+        columns = $stdin.winsize[1]
+        text_length = text.length
+        column_location = columns / 2 - text_length / 2
+        "\e[#{column_location}G#{text}"
+    end
 end
 
 class FileDialogView < BasicView
-    def display
-        puts "Select an Apache log file."
+    def display log_file
+        clear_display
+        set_cursor
+        puts red(center("Select an Apache log file."))
+        update log_file
+    end
+
+    def update log_file
+        set_cursor 2, 1
+        log_file.directory.each_with_index do |directory_entry, index|
+
+            if index < log_file.list_start
+                next
+            end
+
+            if index > log_file.list_start + $stdin.winsize[0] - 3
+                break
+            end
+
+            directory_entry = directory_entry + "/" if Dir.exist? (log_file.file_path + directory_entry)
+            directory_entry = red(directory_entry) if index == log_file.directory_index
+
+            print directory_entry + "\e[K\n"
+        end
+
+        print "\e[J"
+
+        set_cursor $stdin.winsize[0], 1
+
+        print red("Type 'q' to exit; up/down to move; return to select")
+    end
+
+    def quittable?
+        true
     end
 end
 
